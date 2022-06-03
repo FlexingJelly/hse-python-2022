17ec7af53d1630ace15bee0d074ddce622e99b2c
FlexingJelly <104491868+FlexingJelly@users.noreply.github.com>
2022-06-03 22:47:24 +0300

    Update practice4.py

--- tasks/practice4/practice4.py
+++ tasks/practice4/practice4.py
@@ -37,7 +37,43 @@ def search_phone(content: Any, name: str) -> Optional[str]:
     :param name: имя пользователя, у которого будем искать номер телефона
     :return: номер телефона пользователя или None
     """
+    def check_values(content):
+        checked_value = 0
+        values_list = content.values()
+        for value in values_list:
+            print(value)
+            if value == name:
+                checked_value = values_list['phone']
+        return checked_value
 
-    # пиши свой код здесь
+    def search_in_depth(data: Any, name: str):
+        if isinstance(data, dict):
+            searching_area = data.values()
+        else:
+            for part in data:
+                list_search = search_in_depth(part, name)
+                if list_search != 0:
+                    return list_search
+            return 0
+        for part in searching_area:
+            if part == None:
+                return 0
+            if part == name:
+                return searching_area
+            if not isinstance(part, int) and not isinstance(part, str):
+                go_deeper = search_in_depth(part, name)
+                if go_deeper != 0:
+                    return go_deeper
+        return 0
+    phone_number = search_in_depth(content, name)
+    if phone_number != 0:
+        phone_number = str(phone_number)
+        plus_place = phone_number.find('+')
+        phone_number = phone_number[plus_place:]
+        phone_number = phone_number[:12]
+    else:
+        return None
 
-    return None
+
+
+    return phone_number
